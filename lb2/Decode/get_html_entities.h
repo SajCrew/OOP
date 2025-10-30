aa8fec69c32288051de6b213edaee56e9fe70ce0
SajCrew <102975402+SajCrew@users.noreply.github.com>
2025-10-30 11:03:39 +0300

    Delete lb2/Decode directory

--- lb2/Decode/get_html_entities.h
+++
@@ -1,9 +0,0 @@
-#ifndef GET_HTML_ENTITIES_H
-#define GET_HTML_ENTITIES_H
-
-#include <string>
-#include <unordered_map>
-
-using HtmlEntityMap = const std::unordered_map<std::string, char>&;
-const std::unordered_map<std::string, char>& GetHtmlEntities();
-#endif
--- lb2/Decode/html_decode.h
+++
@@ -1,9 +0,0 @@
-#ifndef HTML_DECODE_H
-#define HTML_DECODE_H
-
-#include "get_html_entities.h"
-#include <string>
-
-bool TryDecodeHtmlEntity(const std::string& html, size_t pos, const HtmlEntityMap& htmlEntities, char& decodedChar, size_t& entityLength);
-std::string HtmlDecode(const std::string& html);
-#endif
